aeb7142149d0d05ebc67becd7b8d759de923d152
Fadil Nohur <fadil.nohur@outlook.com>
2022-12-03 23:29:48 +0000

    AoC 2022 Day 2 - reformat file using Black

--- day_02/process.py
+++ day_02/process.py
@@ -15,23 +15,11 @@ class RPSScores(enum.Enum):
     WIN = 6
 
 
-OPPONENT_MOVES = {
-    'A': RPSShapes.ROCK,
-    'B': RPSShapes.PAPER,
-    'C': RPSShapes.SCISSORS
-}
+OPPONENT_MOVES = {"A": RPSShapes.ROCK, "B": RPSShapes.PAPER, "C": RPSShapes.SCISSORS}
 
-SELF_MOVES = {
-    'X': RPSShapes.ROCK,
-    'Y': RPSShapes.PAPER,
-    'Z': RPSShapes.SCISSORS
-}
+SELF_MOVES = {"X": RPSShapes.ROCK, "Y": RPSShapes.PAPER, "Z": RPSShapes.SCISSORS}
 
-OUTCOMES = {
-    'X': RPSScores.LOSS,
-    'Y': RPSScores.DRAW,
-    'Z': RPSScores.WIN
-}
+OUTCOMES = {"X": RPSScores.LOSS, "Y": RPSScores.DRAW, "Z": RPSScores.WIN}
 
 
 @dataclasses.dataclass(frozen=True)
@@ -39,7 +27,9 @@ class RoundPart1:
     opponent_move: RPSShapes
     your_move: RPSShapes
 
-    SHAPE_RELATIONSHIPS = list(itertools.pairwise(list(RPSShapes) + list(RPSShapes)[:1]))
+    SHAPE_RELATIONSHIPS = list(
+        itertools.pairwise(list(RPSShapes) + list(RPSShapes)[:1])
+    )
 
     @property
     def outcome(self) -> RPSScores:
@@ -50,7 +40,7 @@ class RoundPart1:
         elif (self.your_move, self.opponent_move) in self.SHAPE_RELATIONSHIPS:
             return RPSScores.LOSS
         else:
-            raise Exception('Unexpected outcome')
+            raise Exception("Unexpected outcome")
 
 
 @dataclasses.dataclass(frozen=True)
@@ -78,48 +68,59 @@ class RoundPart2:
         ):
             return RPSShapes.SCISSORS
         else:
-            raise Exception('Unexpected move')
+            raise Exception("Unexpected move")
 
 
 class RockPaperScissors:
-
-    def __init__(self, encrypted_strategy_guide: str, right_column_mode: str = 'move'):
+    def __init__(self, encrypted_strategy_guide: str, right_column_mode: str = "move"):
         self.strategy_guide = []
         for round_txt in encrypted_strategy_guide.splitlines():
             round_choices = round_txt.split()
             assert len(round_choices) == 2
             round: RoundPart1 | RoundPart2
-            if right_column_mode == 'move':
+            if right_column_mode == "move":
                 round = RoundPart1(
                     opponent_move=OPPONENT_MOVES[round_choices[0]],
-                    your_move=SELF_MOVES[round_choices[1]]
+                    your_move=SELF_MOVES[round_choices[1]],
                 )
-            elif right_column_mode == 'outcome':
+            elif right_column_mode == "outcome":
                 round = RoundPart2(
                     opponent_move=OPPONENT_MOVES[round_choices[0]],
-                    outcome=OUTCOMES[round_choices[1]]
+                    outcome=OUTCOMES[round_choices[1]],
                 )
             else:
-                raise Exception('Unexpected param')
+                raise Exception("Unexpected param")
             self.strategy_guide.append(round)
 
     def calculate_total_score(self) -> int:
-        return sum((round.your_move.value + round.outcome.value) for round in self.strategy_guide)
+        return sum(
+            (round.your_move.value + round.outcome.value)
+            for round in self.strategy_guide
+        )
 
 
 def read_file() -> str:
-    with open('input.txt') as f:
+    with open("input.txt") as f:
         return f.read()
 
 
 def main() -> None:
     file_txt = read_file()
-    rps_right_column_mode_move = RockPaperScissors(file_txt, right_column_mode='move')
-    print('Total score when right column is move:', rps_right_column_mode_move.calculate_total_score())
-    rps_right_column_mode_outcome = RockPaperScissors(file_txt, right_column_mode='outcome')
-    print('Total score when right column is outcome:', rps_right_column_mode_outcome.calculate_total_score())
+    rps_right_column_mode_move = RockPaperScissors(file_txt, right_column_mode="move")
+    print(
+        "Total score when right column is move:",
+        rps_right_column_mode_move.calculate_total_score(),
+    )
+    rps_right_column_mode_outcome = RockPaperScissors(
+        file_txt, right_column_mode="outcome"
+    )
+    print(
+        "Total score when right column is outcome:",
+        rps_right_column_mode_outcome.calculate_total_score(),
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import timeit
+
     print(timeit.timeit(main, number=1))
