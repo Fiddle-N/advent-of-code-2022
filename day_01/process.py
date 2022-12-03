65863d7818faf9584deec04074774f77f11c294e
Fadil Nohur <fadil.nohur@outlook.com>
2022-12-03 21:08:54 +0000

    AoC 2022 Day 1 - Black format process file

--- day_01/process.py
+++ day_01/process.py
@@ -1,16 +1,16 @@
-
 class CalorieCounting:
-
     def __init__(self, inventory_input: str):
         raw_inventory = [
-            [int(item) for item in elf_items.split('\n')]
-            for elf_items in inventory_input.strip().split('\n\n')
+            [int(item) for item in elf_items.split("\n")]
+            for elf_items in inventory_input.strip().split("\n\n")
         ]
-        self._inventory = sorted([sum(elf_inventory) for elf_inventory in raw_inventory], reverse=True)
+        self._inventory = sorted(
+            [sum(elf_inventory) for elf_inventory in raw_inventory], reverse=True
+        )
 
     @classmethod
-    def read_file(cls) -> 'CalorieCounting':
-        with open('input.txt') as f:
+    def read_file(cls) -> "CalorieCounting":
+        with open("input.txt") as f:
             return cls(f.read())
 
     def calculate_calories_of_elf_carrying_most_calories(self) -> int:
@@ -22,13 +22,17 @@ class CalorieCounting:
 
 def main() -> None:
     cc = CalorieCounting.read_file()
-    print('Total calories of elf carrying most calories:', cc.calculate_calories_of_elf_carrying_most_calories())
     print(
-        'Total calories of top 3 elves carrying most calories:',
-        cc.calculate_calories_of_top_three_elves_carrying_most_calories()
+        "Total calories of elf carrying most calories:",
+        cc.calculate_calories_of_elf_carrying_most_calories(),
+    )
+    print(
+        "Total calories of top 3 elves carrying most calories:",
+        cc.calculate_calories_of_top_three_elves_carrying_most_calories(),
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import timeit
+
     print(timeit.timeit(main, number=1))
