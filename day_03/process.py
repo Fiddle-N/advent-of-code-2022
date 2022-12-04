d23d1775eccfc856ae47f7bca707464c6130471d
Fadil Nohur <fadil.nohur@outlook.com>
2022-12-04 00:08:15 +0000

    AoC 2022 Day 3 - Black refactoring

--- day_03/process.py
+++ day_03/process.py
@@ -15,30 +15,32 @@ class Rucksack:
     def compartments(self) -> list[str]:
         assert len(self.contents) % 2 == 0  # verify even number of items in rucksack
         return [
-            ''.join(compartment)
+            "".join(compartment)
             for compartment in more_itertools.divide(2, self.contents)
         ]
 
 
 class Rucksacks(list[Rucksack]):
-
     def __repr__(self) -> str:
         return f"Rucksacks[{super().__repr__()}]"
 
     @property
     def elf_groups(self) -> list[list[Rucksack]]:
-        assert len(self) % 3 == 0  # verify rucksacks can be split amongst each group of three elves
+        assert (
+            len(self) % 3 == 0
+        )  # verify rucksacks can be split amongst each group of three elves
         return list(more_itertools.chunked(self, n=3))
 
 
 class RucksackReorganisation:
-
     def __init__(self, contents: str) -> None:
-        self._rucksacks = Rucksacks([Rucksack(rucksack_contents) for rucksack_contents in contents.splitlines()])
+        self._rucksacks = Rucksacks(
+            [Rucksack(rucksack_contents) for rucksack_contents in contents.splitlines()]
+        )
 
     @classmethod
     def read_file(cls) -> "RucksackReorganisation":
-        with open('input.txt') as f:
+        with open("input.txt") as f:
             return cls(f.read())
 
     @staticmethod
@@ -49,9 +51,9 @@ class RucksackReorganisation:
                 *[set(contents) for contents in contents_group]
             )
             try:
-                common_item, = common_items
+                (common_item,) = common_items
             except ValueError as exc:
-                raise ValueError('More than one common item retrieved') from exc
+                raise ValueError("More than one common item retrieved") from exc
             sum_priorities += PRIORITIES.index(common_item)
         return sum_priorities
 
@@ -70,15 +72,16 @@ class RucksackReorganisation:
 def main() -> None:
     rr = RucksackReorganisation.read_file()
     print(
-        'Sum priorities of common items in compartments within each rucksack:',
+        "Sum priorities of common items in compartments within each rucksack:",
         rr.sum_priorities_of_common_items_across_rucksack_compartments(),
     )
     print(
-        'Sum priorities of common items across rucksacks in each elf group:',
+        "Sum priorities of common items across rucksacks in each elf group:",
         rr.sum_priorities_of_common_items_across_elf_group_rucksacks(),
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import timeit
+
     print(timeit.timeit(main, number=1))
--- day_03/test.py
+++ day_03/test.py
@@ -6,60 +6,73 @@ from day_03 import process
 @pytest.mark.parametrize(
     "rucksacks, sum_priorities",
     [
-        ('vJrwpWtwJgWrhcsFMMfFFhFp', 16),
-        ('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 38),
-        ('PmmdzqPrVvPwwTWBwg', 42),
-        ('wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 22),
-        ('ttgJtRGJQctTZtZT', 20),
-        ('CrZsJsPPZsGzwwsLwLmpwMDw', 19),
+        ("vJrwpWtwJgWrhcsFMMfFFhFp", 16),
+        ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 38),
+        ("PmmdzqPrVvPwwTWBwg", 42),
+        ("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", 22),
+        ("ttgJtRGJQctTZtZT", 20),
+        ("CrZsJsPPZsGzwwsLwLmpwMDw", 19),
         (
-                """\
+            """\
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw""", 157
-        )
-    ]
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            157,
+        ),
+    ],
 )
-def test_rucksack_reorganisation_sum_priorities_of_common_items_across_rucksack_compartments(rucksacks: str, sum_priorities: int) -> None:
+def test_rucksack_reorganisation_sum_priorities_of_common_items_across_rucksack_compartments(
+    rucksacks: str, sum_priorities: int
+) -> None:
     contents = f"""\
 {rucksacks}
 """
     rr = process.RucksackReorganisation(contents)
-    assert rr.sum_priorities_of_common_items_across_rucksack_compartments() == sum_priorities
+    assert (
+        rr.sum_priorities_of_common_items_across_rucksack_compartments()
+        == sum_priorities
+    )
 
 
 @pytest.mark.parametrize(
     "rucksacks, sum_priorities",
     [
         (
-                """\
+            """\
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg""", 18
+PmmdzqPrVvPwwTWBwg""",
+            18,
         ),
         (
-                """\
+            """\
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw""", 52
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            52,
         ),
         (
-                """\
+            """\
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw""", 70
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            70,
         ),
-    ]
+    ],
 )
-def test_rucksack_reorganisation_sum_priorities_of_common_items_across_elf_group_rucksacks(rucksacks: str, sum_priorities: int) -> None:
+def test_rucksack_reorganisation_sum_priorities_of_common_items_across_elf_group_rucksacks(
+    rucksacks: str, sum_priorities: int
+) -> None:
     contents = f"""\
 {rucksacks}
 """
     rr = process.RucksackReorganisation(contents)
-    assert rr.sum_priorities_of_common_items_across_elf_group_rucksacks() == sum_priorities
+    assert (
+        rr.sum_priorities_of_common_items_across_elf_group_rucksacks() == sum_priorities
+    )
