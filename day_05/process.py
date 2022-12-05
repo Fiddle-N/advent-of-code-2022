6578c11d24c8f557406337b4c5136dd1db30bb23
Fadil Nohur <fadil.nohur@outlook.com>
2022-12-05 12:07:36 +0000

    AoC 2022 Day 5

---
+++ day_05/process.py
@@ -0,0 +1,105 @@
+import dataclasses
+from typing import Optional, Literal, Iterator
+
+import parse  # type: ignore
+
+
+def read_file() -> str:
+    with open("input.txt") as f:
+        return f.read()
+
+
+class CrateStacks(list[Optional[list[str]]]):
+    @classmethod
+    def from_text(cls, stack_txt: str) -> "CrateStacks":
+        stack_list = reversed(stack_txt.splitlines())
+        header, stack_contents = next(stack_list), list(stack_list)
+        assert (
+            max(int(crate_no) for crate_no in header.strip().split()) <= 9
+        )  # Only a single-digit number of stacks are supported
+        stack_text_indices = [
+            (int(ele), idx) for idx, ele in enumerate(header) if ele != " "
+        ]
+        stacks: list[Optional[list[str]]] = [None] + [[] for _ in stack_text_indices]
+
+        for row in stack_contents:
+            for stack_index, stack_text_index in stack_text_indices:
+                stack_value = row[stack_text_index]
+                if stack_value != " ":  # found a stack value
+                    stack = stacks[stack_index]
+                    assert stack is not None
+                    stack.append(stack_value)
+        return cls(stacks)
+
+    @property
+    def end_crates(self) -> str:
+        return "".join(stack[-1] for stack in self if stack is not None)
+
+
+@dataclasses.dataclass
+class Rearrangement:
+    moves: int
+    start: int
+    end: int
+
+    @classmethod
+    def from_text(cls, rearrangement_txt: str) -> "Rearrangement":
+        parsed_text = parse.parse(
+            "move {move:d} from {start:d} to {end:d}", rearrangement_txt
+        )
+        return cls(parsed_text["move"], parsed_text["start"], parsed_text["end"])
+
+
+class SupplyStacks:
+    def __init__(self, input_txt: str, crane_type: Literal["9000", "9001"]) -> None:
+        stacks_txt, rearrangements_txt = input_txt.split("\n\n")
+        self.crate_stacks = CrateStacks.from_text(stacks_txt)
+        self.rearrangement_proc = [
+            Rearrangement.from_text(rearrangement_txt)
+            for rearrangement_txt in rearrangements_txt.splitlines()
+        ]
+        self.crane_type = crane_type
+
+    def _rearrange(self, rearrangement: Rearrangement) -> CrateStacks:
+        start_stack = self.crate_stacks[rearrangement.start]
+        end_stack = self.crate_stacks[rearrangement.end]
+        assert start_stack is not None
+        assert end_stack is not None
+        picked_up = []
+        for _ in range(rearrangement.moves):
+            picked_up.append(start_stack.pop())
+        if self.crane_type == "9000":
+            dropped_off = picked_up
+        elif self.crane_type == "9001":
+            dropped_off = list(reversed(picked_up))
+        else:
+            raise Exception("Unexpected crane mode")
+        end_stack.extend(dropped_off)
+        return self.crate_stacks
+
+    def __iter__(self) -> Iterator[CrateStacks]:
+        for rearrangement in self.rearrangement_proc:
+            yield self._rearrange(rearrangement)
+
+
+def _run_crane(file_txt: str, crane_type: Literal["9000", "9001"]) -> None:
+    ss = SupplyStacks(file_txt, crane_type=crane_type)
+    for _ in iter(ss):
+        pass
+    print(
+        f"End crates after rearrangement procedure with CraneMover {crane_type}:",
+        ss.crate_stacks.end_crates,
+    )
+
+
+def main() -> None:
+    file_txt = read_file()
+
+    _run_crane(file_txt, "9000")
+    _run_crane(file_txt, "9001")
+
+
+if __name__ == "__main__":
+    import timeit
+
+    print(timeit.timeit(main, number=1))
---
+++ day_05/test.py
@@ -0,0 +1,196 @@
+import pytest
+
+from day_05 import process
+
+
+def test_crate_stacks() -> None:
+    crate_stacks_txt = """\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+"""
+    crate_stacks = process.CrateStacks.from_text(crate_stacks_txt)
+    assert crate_stacks == process.CrateStacks(
+        [
+            None,
+            ["Z", "N"],
+            ["M", "C", "D"],
+            ["P"],
+        ]
+    )
+    assert crate_stacks.end_crates == "NDP"
+
+
+def test_rearrangement() -> None:
+    rearrangement_txt = "move 1 from 2 to 1"
+    rearrangement = process.Rearrangement.from_text(rearrangement_txt)
+    assert rearrangement == process.Rearrangement(
+        moves=1,
+        start=2,
+        end=1,
+    )
+
+
+def test_starting_stacks_cranemover_9000() -> None:
+    input_txt = """\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+    ss = process.SupplyStacks(input_txt, crane_type="9000")
+    assert ss.crate_stacks == process.CrateStacks(
+        [
+            None,
+            ["Z", "N"],
+            ["M", "C", "D"],
+            ["P"],
+        ]
+    )
+    assert ss.rearrangement_proc == [
+        process.Rearrangement(
+            moves=1,
+            start=2,
+            end=1,
+        ),
+        process.Rearrangement(
+            moves=3,
+            start=1,
+            end=3,
+        ),
+        process.Rearrangement(
+            moves=2,
+            start=2,
+            end=1,
+        ),
+        process.Rearrangement(
+            moves=1,
+            start=1,
+            end=2,
+        ),
+    ]
+    ss_iter = iter(ss)
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["Z", "N", "D"],
+            ["M", "C"],
+            ["P"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            [],
+            ["M", "C"],
+            ["P", "D", "N", "Z"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["C", "M"],
+            [],
+            ["P", "D", "N", "Z"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["C"],
+            ["M"],
+            ["P", "D", "N", "Z"],
+        ]
+    )
+    with pytest.raises(StopIteration):
+        next(ss_iter)
+
+    assert ss.crate_stacks.end_crates == "CMZ"
+
+
+def test_starting_stacks_cranemover_9001() -> None:
+    input_txt = """\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+    ss = process.SupplyStacks(input_txt, crane_type="9001")
+    assert ss.crate_stacks == process.CrateStacks(
+        [
+            None,
+            ["Z", "N"],
+            ["M", "C", "D"],
+            ["P"],
+        ]
+    )
+    assert ss.rearrangement_proc == [
+        process.Rearrangement(
+            moves=1,
+            start=2,
+            end=1,
+        ),
+        process.Rearrangement(
+            moves=3,
+            start=1,
+            end=3,
+        ),
+        process.Rearrangement(
+            moves=2,
+            start=2,
+            end=1,
+        ),
+        process.Rearrangement(
+            moves=1,
+            start=1,
+            end=2,
+        ),
+    ]
+    ss_iter = iter(ss)
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["Z", "N", "D"],
+            ["M", "C"],
+            ["P"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            [],
+            ["M", "C"],
+            ["P", "Z", "N", "D"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["M", "C"],
+            [],
+            ["P", "Z", "N", "D"],
+        ]
+    )
+    assert next(ss_iter) == process.CrateStacks(
+        [
+            None,
+            ["M"],
+            ["C"],
+            ["P", "Z", "N", "D"],
+        ]
+    )
+    with pytest.raises(StopIteration):
+        next(ss_iter)
+
+    assert ss.crate_stacks.end_crates == "MCD"
