e456593fa3baa8bb513f27a44d8170b7891697e9
Fadil Nohur <fadil.nohur@outlook.com>
2022-12-09 01:42:34 +0000

    AoC 2022 Day 8

---
+++ day_08/process.py
@@ -0,0 +1,134 @@
+import dataclasses
+import enum
+import io
+import math
+from typing import Callable, Iterator
+
+import numpy as np
+import numpy.typing as npt
+
+
+class Direction(enum.Enum):
+    LEFT = enum.auto()
+    RIGHT = enum.auto()
+    TOP = enum.auto()
+    BOTTOM = enum.auto()
+
+
+@dataclasses.dataclass
+class TreeDirectionDetails:
+    view_distance: int
+    is_visible: bool
+
+
+@dataclasses.dataclass(frozen=True)
+class Coords:
+    x: int
+    y: int
+
+
+class Tree(dict[Direction, TreeDirectionDetails]):
+    def __repr__(self) -> str:
+        return f"Tree({super().__repr__})"
+
+    @property
+    def is_visible(self) -> bool:
+        return any(
+            [direction_details.is_visible for direction_details in self.values()]
+        )
+
+    @property
+    def scenic_score(self) -> int:
+        return math.prod(
+            [direction_details.view_distance for direction_details in self.values()]
+        )
+
+
+class TreetopTreeHouse:
+    def __init__(self, tree_height_map: str) -> None:
+        self._tree_map = self._create_tree_map(tree_height_map)
+        self.trees = self._create_trees()
+
+    @classmethod
+    def read_file(cls) -> "TreetopTreeHouse":
+        with open("input.txt") as f:
+            return cls(f.read())
+
+    @staticmethod
+    def _create_tree_map(tree_height_map: str) -> npt.NDArray[np.int32]:
+        f = io.StringIO(tree_height_map)
+        first_line = f.readline().strip()
+        f.seek(0)
+        return np.genfromtxt(f, dtype="i4", delimiter=[1] * len(first_line))
+
+    @staticmethod
+    def _create_tree_details(
+        current_tree_height: int,
+        direction: Direction,
+        directional_trees: npt.NDArray[np.int32],
+    ) -> TreeDirectionDetails:
+        if direction in (Direction.LEFT, Direction.TOP):
+            directional_trees = np.flip(directional_trees)
+        view_distance = 0
+        for view_distance, tree_height in enumerate(directional_trees, start=1):
+            if tree_height >= current_tree_height:
+                return TreeDirectionDetails(
+                    view_distance=view_distance, is_visible=False
+                )
+        return TreeDirectionDetails(
+            view_distance=view_distance, is_visible=True
+        )  # no trees can be found
+
+    def _create_tree(self, row: int, col: int) -> Tree:
+        tree_height = self._tree_map[row, col]
+        directions = {
+            Direction.LEFT: self._tree_map[row, :col],
+            Direction.RIGHT: self._tree_map[row, col + 1 :],
+            Direction.TOP: self._tree_map[:row, col],
+            Direction.BOTTOM: self._tree_map[row + 1 :, col],
+        }
+        tree = Tree(
+            {
+                direction: self._create_tree_details(
+                    tree_height, direction, directional_trees
+                )
+                for direction, directional_trees in directions.items()
+            }
+        )
+        return tree
+
+    def _create_trees(self) -> dict[Coords, Tree]:
+        trees = {}
+        tree_map_iter = np.nditer(self._tree_map, flags=["multi_index"])
+        for _ in tree_map_iter:
+            row, col = tree_map_iter.multi_index
+            tree = self._create_tree(row, col)
+            trees[Coords(col, row)] = tree
+        return trees
+
+    def _treehouse_query(self, fn: Callable[[Iterator[int]], int], query: str) -> int:
+        return fn(getattr(tree, query) for tree in self.trees.values())
+
+    def sum_visible_trees(self) -> int:
+        return self._treehouse_query(sum, "is_visible")
+
+    def max_scenic_score(self) -> int:
+        return self._treehouse_query(max, "scenic_score")
+
+
+def main() -> None:
+    tth = TreetopTreeHouse.read_file()
+    print(
+        "Sum of visible trees from outside the grid:",
+        tth.sum_visible_trees(),
+    )
+    print(
+        "Highest scenic score possible for any tree:",
+        tth.max_scenic_score(),
+    )
+
+
+if __name__ == "__main__":
+    import timeit
+
+    print(timeit.timeit(main, number=1))
---
+++ day_08/test.py
@@ -0,0 +1,274 @@
+import pytest
+
+from day_08 import process
+
+
+@pytest.mark.parametrize(
+    "coords, directional_assertions, overall_assertion",
+    [
+        # four corners - assert visible from beyond edges
+        (
+            process.Coords(x=0, y=0),
+            (
+                (process.Direction.LEFT, True),
+                (process.Direction.TOP, True),
+            ),
+            True,
+        ),
+        (
+            process.Coords(x=4, y=0),
+            (
+                (process.Direction.RIGHT, True),
+                (process.Direction.TOP, True),
+            ),
+            True,
+        ),
+        (
+            process.Coords(x=0, y=4),
+            (
+                (process.Direction.LEFT, True),
+                (process.Direction.BOTTOM, True),
+            ),
+            True,
+        ),
+        (
+            process.Coords(x=4, y=4),
+            (
+                (process.Direction.RIGHT, True),
+                (process.Direction.BOTTOM, True),
+            ),
+            True,
+        ),
+        # four borders (that are not corners) - assert visible from beyond edge
+        ## top
+        (
+            process.Coords(x=1, y=0),
+            ((process.Direction.TOP, True),),
+            True,
+        ),
+        (
+            process.Coords(x=2, y=0),
+            ((process.Direction.TOP, True),),
+            True,
+        ),
+        (
+            process.Coords(x=3, y=0),
+            ((process.Direction.TOP, True),),
+            True,
+        ),
+        ## bottom
+        (
+            process.Coords(x=1, y=4),
+            ((process.Direction.BOTTOM, True),),
+            True,
+        ),
+        (
+            process.Coords(x=2, y=4),
+            ((process.Direction.BOTTOM, True),),
+            True,
+        ),
+        (
+            process.Coords(x=3, y=4),
+            ((process.Direction.BOTTOM, True),),
+            True,
+        ),
+        ## left
+        (
+            process.Coords(x=0, y=1),
+            ((process.Direction.LEFT, True),),
+            True,
+        ),
+        (
+            process.Coords(x=0, y=2),
+            ((process.Direction.LEFT, True),),
+            True,
+        ),
+        (
+            process.Coords(x=0, y=3),
+            ((process.Direction.LEFT, True),),
+            True,
+        ),
+        ## right
+        (
+            process.Coords(x=4, y=1),
+            ((process.Direction.RIGHT, True),),
+            True,
+        ),
+        (
+            process.Coords(x=4, y=2),
+            ((process.Direction.RIGHT, True),),
+            True,
+        ),
+        (
+            process.Coords(x=4, y=3),
+            ((process.Direction.RIGHT, True),),
+            True,
+        ),
+        # interior nine trees
+        ## top-left
+        (
+            process.Coords(x=1, y=1),
+            (
+                (process.Direction.LEFT, True),
+                (process.Direction.RIGHT, False),
+                (process.Direction.TOP, True),
+                (process.Direction.BOTTOM, False),
+            ),
+            True,
+        ),
+        ## top-middle
+        (
+            process.Coords(x=2, y=1),
+            (
+                (process.Direction.LEFT, False),
+                (process.Direction.RIGHT, True),
+                (process.Direction.TOP, True),
+                (process.Direction.BOTTOM, False),
+            ),
+            True,
+        ),
+        ## top-right
+        (
+            process.Coords(x=3, y=1),
+            (
+                (process.Direction.LEFT, False),
+                (process.Direction.RIGHT, False),
+                (process.Direction.TOP, False),
+                (process.Direction.BOTTOM, False),
+            ),
+            False,
+        ),
+        ## left-middle
+        (
+            process.Coords(x=1, y=2),
+            (
+                (process.Direction.LEFT, False),
+                (process.Direction.RIGHT, True),
+                (process.Direction.TOP, False),
+                (process.Direction.BOTTOM, False),
+            ),
+            True,
+        ),
+        ## centre
+        (
+            process.Coords(x=2, y=2),
+            (
+                (process.Direction.LEFT, False),
+                (process.Direction.RIGHT, False),
+                (process.Direction.TOP, False),
+                (process.Direction.BOTTOM, False),
+            ),
+            False,
+        ),
+        ## right-middle
+        (
+            process.Coords(x=3, y=2),
+            (
+                (process.Direction.LEFT, False),
+                (process.Direction.RIGHT, True),
+                (process.Direction.TOP, False),
+                (process.Direction.BOTTOM, False),
+            ),
+            True,
+        ),
+        ## bottom-left
+        (
+            process.Coords(x=1, y=3),
+            (),
+            False,
+        ),
+        ## bottom-middle
+        (
+            process.Coords(x=2, y=3),
+            (),
+            True,
+        ),
+        ## bottom-right
+        (
+            process.Coords(x=3, y=3),
+            (),
+            False,
+        ),
+    ],
+)
+def test_treetop_tree_house_is_visible(
+    coords: process.Coords,
+    directional_assertions: tuple[tuple[process.Direction, bool]],
+    overall_assertion: bool,
+) -> None:
+    map_ = """\
+30373
+25512
+65332
+33549
+35390"""
+    tth = process.TreetopTreeHouse(map_)
+    tree = tth.trees[coords]
+    for direction, directional_assertion in directional_assertions:
+        assert tree[direction].is_visible == directional_assertion
+    assert tree.is_visible == overall_assertion
+
+
+def test_treetop_tree_house_sum_visible_trees() -> None:
+    map_ = """\
+30373
+25512
+65332
+33549
+35390"""
+    tth = process.TreetopTreeHouse(map_)
+    assert tth.sum_visible_trees() == 21
+
+
+@pytest.mark.parametrize(
+    "coords, directional_assertions, overall_assertion",
+    [
+        (
+            process.Coords(x=2, y=1),
+            (
+                (process.Direction.LEFT, 1),
+                (process.Direction.RIGHT, 2),
+                (process.Direction.TOP, 1),
+                (process.Direction.BOTTOM, 2),
+            ),
+            4,
+        ),
+        (
+            process.Coords(x=2, y=3),
+            (
+                (process.Direction.LEFT, 2),
+                (process.Direction.RIGHT, 2),
+                (process.Direction.TOP, 2),
+                (process.Direction.BOTTOM, 1),
+            ),
+            8,
+        ),
+    ],
+)
+def test_treetop_tree_house_scenic_score(
+    coords: process.Coords,
+    directional_assertions: tuple[tuple[process.Direction, bool]],
+    overall_assertion: bool,
+) -> None:
+    map_ = """\
+30373
+25512
+65332
+33549
+35390"""
+    tth = process.TreetopTreeHouse(map_)
+    tree = tth.trees[coords]
+    for direction, directional_assertion in directional_assertions:
+        assert tree[direction].view_distance == directional_assertion
+    assert tree.scenic_score == overall_assertion
+
+
+def test_treetop_tree_house_max_scenic_score() -> None:
+    map_ = """\
+30373
+25512
+65332
+33549
+35390"""
+    tth = process.TreetopTreeHouse(map_)
+    assert tth.max_scenic_score() == 8
